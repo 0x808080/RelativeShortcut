70c13fb087dca3d99961e84ba6d8f00d68e921bb
Lenovo X230 <Lenovo@X230>
2018-08-06 18:03:20 +0900

    メインフォームを変更

--- RelativeShortcut/Forms/MainForm.Designer.cs
+++ RelativeShortcut/Forms/MainForm.Designer.cs
@@ -29,13 +29,19 @@
 		{
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ツールTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ContextGroupBox = new System.Windows.Forms.GroupBox();
+			this.menuStrip1.SuspendLayout();
+			this.ContextGroupBox.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Location = new System.Drawing.Point(6, 18);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(163, 23);
+			this.button1.Size = new System.Drawing.Size(148, 23);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "シェル拡張";
 			this.button1.UseVisualStyleBackColor = true;
@@ -43,25 +49,64 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(12, 41);
+			this.button2.Location = new System.Drawing.Point(6, 47);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(163, 23);
+			this.button2.Size = new System.Drawing.Size(148, 23);
 			this.button2.TabIndex = 1;
 			this.button2.Text = "シェル拡張　解除";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.ツールTToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// ファイルFToolStripMenuItem
+			// 
+			this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+			this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+			this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+			// 
+			// ツールTToolStripMenuItem
+			// 
+			this.ツールTToolStripMenuItem.Name = "ツールTToolStripMenuItem";
+			this.ツールTToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+			this.ツールTToolStripMenuItem.Text = "ツール(&T)";
+			// 
+			// ContextGroupBox
+			// 
+			this.ContextGroupBox.Controls.Add(this.button1);
+			this.ContextGroupBox.Controls.Add(this.button2);
+			this.ContextGroupBox.Location = new System.Drawing.Point(21, 27);
+			this.ContextGroupBox.Name = "ContextGroupBox";
+			this.ContextGroupBox.Size = new System.Drawing.Size(160, 86);
+			this.ContextGroupBox.TabIndex = 3;
+			this.ContextGroupBox.TabStop = false;
+			this.ContextGroupBox.Text = "コンテキスト";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.ContextGroupBox);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "0000";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ContextGroupBox.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -69,6 +114,10 @@
 
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ツールTToolStripMenuItem;
+		private System.Windows.Forms.GroupBox ContextGroupBox;
 	}
 }
 
