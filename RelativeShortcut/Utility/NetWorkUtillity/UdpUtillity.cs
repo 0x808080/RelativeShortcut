c24126b19b8bbbf84542dde55372ba3b486b276d
Lenovo X230 <Lenovo@X230>
2018-08-21 17:31:20 +0900

    UDP通信部の作成

--- RelativeShortcut/Utility/NetWorkUtillity/UdpUtillity.cs
+++ RelativeShortcut/Utility/NetWorkUtillity/UdpUtillity.cs
@@ -16,8 +16,8 @@ namespace Utility
 		/// <summary>呼び出し元へ伝えるイベントハンドラ</summary>
 		public event UdpRecvEventHandler UdpRecvEvent;
 
-		/// <summary>送信先のエンドポイント</summary>
-		private readonly IPEndPoint _endpoint;
+		/// <summary>受信時のエンドポイント</summary>
+		private IPEndPoint _endpoint;
 
 		/// <summary>受信待機中のUDPオブジェクト</summary>
 		private UdpClient udpClient = null;
@@ -26,20 +26,22 @@ namespace Utility
 		/// <summary>
 		/// コンストラクタ
 		/// </summary>
-		/// <param name="port">受信ポート番号</param>
 		/// *******************************************************************
-		public UdpUtillity(int port)
+		public UdpUtillity()
 		{
-			_endpoint = new IPEndPoint( IPAddress.Any, port );
+
 		}
 
 		/// *******************************************************************
 		/// <summary>
 		/// 非同期受信の開始
 		/// </summary>
+		/// <param name="recvPort">受信ポート番号</param>
 		/// *******************************************************************
-		public void ReceiveStart()
+		public void ReceiveStart(int recvPort)
 		{
+			_endpoint = new IPEndPoint( IPAddress.Any, recvPort );
+
 			// UdpClientを作成しバインド
 			udpClient = new UdpClient( _endpoint );
 
@@ -72,12 +74,86 @@ namespace Utility
 			}
 
 			// 呼び出し元へ通知
-			UdpRecvEvent( new UdpRecvEventArgs( rcvBytes ) );
+			UdpRecvEvent?.Invoke( new UdpRecvEventArgs( rcvBytes, remoteEP ) );
 
 			// 非同期受信を再開
 			udp.BeginReceive( ReceiveCallback, udp );
 		}
 
+		/// *******************************************************************
+		/// <summary>
+		/// IPアドレスとポートからエンドポイントの作成
+		/// </summary>
+		/// <param name="ipaddr">IPアドレス</param>
+		/// <param name="port">ポート番号</param>
+		/// <returns>エンドポイント</returns>
+		/// *******************************************************************
+		public IPEndPoint GetEndPoint(string ipaddr, int port)
+		{
+			IPAddress addr = null;
+
+			try {
+				addr = IPAddress.Parse( ipaddr );
+			} catch( FormatException e ) {
+				MsgUtillity.ShowMsg( e.Message, MsgType.ERR );
+				return null;
+			}
+
+			IPEndPoint endPoint = new IPEndPoint( addr, port );
+
+			return endPoint;
+		}
+
+		/// *******************************************************************
+		/// <summary>
+		/// 送信開始
+		/// </summary>
+		/// <param name="sendData">送信データ</param>
+		/// *******************************************************************
+		public void SendStart(byte[] sendData, IPEndPoint endPoint)
+		{
+			// UdpClientを作成する
+			if( udpClient == null ) {
+				udpClient = new UdpClient();
+			}
+
+			//非同期的にデータを送信する
+			udpClient.BeginSend( sendData, sendData.Length, endPoint, SendCallback, udpClient );
+		}
+
+		/// *******************************************************************
+		/// <summary>
+		/// 送信完了コールバック
+		/// </summary>
+		/// <param name="ar"></param>
+		/// *******************************************************************
+		private void SendCallback(IAsyncResult ar)
+		{
+			UdpClient udp = (UdpClient)ar.AsyncState;
+
+			// 非同期送信を終了する
+			try {
+				udp.EndSend( ar );
+			} catch( SocketException ex ) {
+				Console.WriteLine( "送信エラー({0}/{1})", ex.Message, ex.ErrorCode );
+			} catch( ObjectDisposedException ex ) {
+				//すでに閉じている時は終了
+				Console.WriteLine( "Socketは閉じられています。" );
+			}
+		}
+
+		/// *******************************************************************
+		/// <summary>
+		/// ソケットを
+		/// </summary>
+		/// *******************************************************************
+		public void SocektStop()
+		{
+			//UdpClientを閉じる
+			if( udpClient != null ) {
+				udpClient.Close();
+			}
+		}
 
 		/// *******************************************************************
 		/// <summary>
@@ -86,8 +162,10 @@ namespace Utility
 		/// *******************************************************************
 		public class UdpRecvEventArgs : EventArgs
 		{
-			/// <summary>呼び出し元へのパラメータ</summary>
-			private readonly byte[] _TestNumValue;
+			/// <summary>呼び出し元へのパラメータ(受信データ)</summary>
+			private readonly byte[] _recvData;
+			/// <summary>呼び出し元へのパラメータ(送信元の情報)</summary>
+			private readonly IPEndPoint _endPoint;
 
 			/// *******************************************************************
 			/// <summary>
@@ -95,13 +173,16 @@ namespace Utility
 			/// </summary>
 			/// <param name="TestNumValue"></param>
 			/// *******************************************************************
-			public UdpRecvEventArgs(byte[] recvData)
+			public UdpRecvEventArgs(byte[] recvData, IPEndPoint endPoint)
 			{
-				_TestNumValue = recvData;
+				_recvData = recvData;
+				_endPoint = endPoint;
 			}
 
-			/// <summary>結果</summary>
-			public byte[] Result { get { return _TestNumValue; } }
+			/// <summary>受信データ</summary>
+			public byte[] Data { get { return _recvData; } }
+			/// <summary>送信元情報</summary>
+			public IPEndPoint SrcIndo { get { return _endPoint; } }
 		}
 	}
 }
